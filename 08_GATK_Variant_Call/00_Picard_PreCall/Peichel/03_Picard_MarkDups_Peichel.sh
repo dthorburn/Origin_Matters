d4f97a5184f5843c27a3d7514f4aa86e727d67a2
Doko-Miles Thorburn <35698678+dthorburn@users.noreply.github.com>
2022-02-24 16:03:39 +0000

    Add files via upload

---
+++ 08_GATK_Variant_Call/00_Picard_PreCall/Peichel/03_Picard_MarkDups_Peichel.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#$ -N Picard_Markdups_Peichel
+#$ -j y
+#$ -cwd
+#$ -t 1-66
+#$ -l h_rt=4:00:00
+#$ -l highmem
+#$ -l h_vmem=32G
+
+module load gatk
+
+DATA_DIR=/data/scratch/btx316/Comp_Genomics/03_Picard_Cleaning/Peichel/03_Merged
+#DATA_DIR=/data/scratch/btx316/Comp_Genomics/03_Picard_Cleaning/Gynogene/03_Merged
+OUT_DIR=/data/scratch/btx316/Comp_Genomics/03_Picard_Cleaning/Peichel/04_MarkDups
+#OUT_DIR=/data/scratch/btx316/Comp_Genomics/03_Picard_Cleaning/Gynogene/04_MarkDups
+query_file=BS"${SGE_TASK_ID}".cleaned.sorted.merged.peichel.bam
+#query_file=BS"${SGE_TASK_ID}".cleaned.sorted.merged.gynogene.bam
+newname=`basename $query_file | sed -e "s/.bam/.markdup.bam/"`
+metricsname=`basename $query_file | sed -e "s/.bam/.markdup.txt/"`
+
+gatk MarkDuplicates \
+  -I $DATA_DIR/"$query_file" \
+  -O $OUT_DIR/"$newname" \
+  -M $OUT_DIR/"$metricsname"
+  
+echo "Step 1 done"
+
+gatk BuildBamIndex \
+  -I $OUT_DIR/"$newname"
+  
+echo "All Done"
